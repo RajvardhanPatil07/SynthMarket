b92120b2fc02eaf251c702263ea4fcc7569ed605
Rajvardhan Patil <rajvardhanpatil7890@gmail.com>
2026-08-18 20:47:45 +0530

    fix: satisfy CI lint checks

--- synthmarket/__init__.py
+++ synthmarket/__init__.py
@@ -28,6 +28,7 @@ __all__ = [
     "MultiAssetSyntheticMarketGenerator",
     "PortfolioSpec",
     "SMACrossoverConfig",
+    "StrategySpec",
     "SyntheticModel",
     "SyntheticMarketGenerator",
     "TemporalSplit",
